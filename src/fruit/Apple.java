1e20cdbfacdff74b7a109a701d3022b0c03d8769
bbakjjii <jiyoung920301@gmail.com>
2022-12-05 14:35:23 +0900

    Add ate method Apple

--- src/fruit/Apple.java
+++ src/fruit/Apple.java
@@ -4,7 +4,13 @@ public class Apple {
 	int price;
 	int grade;
 	
-	public void eat() {
-		price -= 1000;
+	boolean clean;
+	
+	public void ate() {
+		price -= 800;
+	}
+	
+	public void clean() {
+		clean = true;
 	}
 }
