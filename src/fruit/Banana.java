5c071384b42e8ba56f8b13bc9e7cfa52945a472a
bbakjjii <jiyoung920301@gmail.com>
2022-12-05 14:26:09 +0900

    Revert "Banana 수정"
    This reverts commit 534a852eca2eb28e96fd968e8c62d96812d4f177.

--- src/fruit/Banana.java
+++ src/fruit/Banana.java
@@ -3,5 +3,14 @@ package fruit;
 public class Banana {
 	int price;
 	int grade;
-
+	
+	boolean clean;
+	
+	public void ate() {
+		price -= 800;
+	}
+	
+	public void clean() {
+		clean = true;
+	}
 }
