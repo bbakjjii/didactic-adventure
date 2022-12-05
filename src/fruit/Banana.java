534a852eca2eb28e96fd968e8c62d96812d4f177
bbakjjii <jiyoung920301@gmail.com>
2022-12-05 14:11:58 +0900

    Banana 수정

--- src/fruit/Banana.java
+++ src/fruit/Banana.java
@@ -3,14 +3,5 @@ package fruit;
 public class Banana {
 	int price;
 	int grade;
-	
-	boolean clean;
-	
-	public void ate() {
-		price -= 800;
-	}
-	
-	public void clean() {
-		clean = true;
-	}
+
 }
